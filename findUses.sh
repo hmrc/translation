07b913a5cad41d50f77d46521d360a8c2dfc52cc
Graeme Bullimore <graeme.bullimore@digital.hmrc.gov.uk>
2017-08-07 10:24:23 +0100

    Updated user prompts

--- findUses.sh
+++ findUses.sh
@@ -18,9 +18,9 @@ then
     rm $NOTFOUNDFILE
 fi
 
-while read -r name value
+while read -r NAME VALUE
 do
-    if [[ ! -z "${name// }" ]] && [[ $name != \#* ]]; then
+    if [[ ! -z "${NAME// }" ]] && [[ $NAME != \#* ]]; then
         MESSAGECOUNT=$[$MESSAGECOUNT +1]
         echo -en "\rCounting records to process: $MESSAGECOUNT"
     fi
@@ -30,14 +30,14 @@ echo
 
 
 
-while read -r name value
+while read -r NAME VALUE
 do
-    if [[ ! -z "${name// }" ]] && [[ $name != \#* ]]; then
+    if [[ ! -z "${NAME// }" ]] && [[ $NAME != \#* ]]; then
        STRINGCOUNT=$[$STRINGCOUNT +1]
-       xname="${name//+([[:space:]])/}"
+       xname="${NAME//+([[:space:]])/}"
        found=$(grep -r --include=*.{html,htm,scala} "$xname")
        if [[ -z "${found// }" ]]; then
-          echo $name >> $NOTFOUNDFILE
+          echo $NAME >> $NOTFOUNDFILE
           NOTFOUNDCOUNTER=$[$NOTFOUNDCOUNTER +1]
           #echo -e ".\c"
           echo -en "\rRecords processed: $STRINGCOUNT / $MESSAGECOUNT"
@@ -53,10 +53,11 @@ echo out of $STRINGCOUNT messages
 echo
 echo
 echo Checking messages which were not directly referenced in the source code, to see if the key may be created at execution time.
-echo Messages that I really think are not used any more will be logged to $SURENOTUSED
-echo Messages that arent used, but their parent is \(i.e. may be concatenated at run time\) are logged to $MAYBENOTUSED
-echo I assume that the delimiter between key sections is a full stop. e.g. site.new.invalidDate has three sections
-echo if you have dynamic keys that are based on a grandparent \(two dynamic sections\), then I will not find them. Please check them yourself. :oP
+echo Messages that I really think are not used any more will be logged to $SURENOTUSED.
+echo Messages that aren\'t used, but their parent is \(i.e. might be concatenated at run time\) are logged to $MAYBENOTUSED.
+echo I assume that the delimiter between key sections is a full stop. e.g. site.new.invalidDate has three sections.
+echo If a key has $MINIMUM_MESSAGE_SECTIONS or fewer sections, I will not check for dynamically concatenated keys, and will just assume that it isn\'t used.
+echo if you have dynamic keys that are based on a grandparent \(two or more dynamic sections\), then I will not find them. Please check them yourself. :oP
 echo
 
 MAYBENOTUSEDCOUNTER=0
@@ -72,35 +73,35 @@ then
     rm $MAYBENOTUSED
 fi
 
-while read -r name
+while read -r NAME
 do
    IFS=$MESSAGE_DELIMITER
-   tokens=( $name )
+   tokens=( $NAME )
    IFS='='
    TOKEN_COUNT=${#tokens[@]}
    LAST_TOKEN=${tokens[$[$TOKEN_COUNT-1]]}
    LEN_LAST_TOKEN=${#LAST_TOKEN}
-   NAME_LEN=${#name}
+   NAME_LEN=${#NAME}
    REMAINING_LEN=$[$NAME_LEN-$LEN_LAST_TOKEN-1]
-   REMAINING=${name:0:$REMAINING_LEN}
+   REMAINING=${NAME:0:$REMAINING_LEN}
    SEARCHSTRING="$REMAINING.\""
 
    if [ "$TOKEN_COUNT" -gt $MINIMUM_MESSAGE_SECTIONS ]; then
       found=$(grep -r --include=*.{html,htm,scala} "$SEARCHSTRING")
       if [[ -z "${found// }" ]]; then
          SURENOTUSEDCOUNTER=$[$SURENOTUSEDCOUNTER +1]
-         echo $name >> $SURENOTUSED
+         echo $NAME >> $SURENOTUSED
       else
          MAYBENOTUSEDCOUNTER=$[$MAYBENOTUSEDCOUNTER +1]
-         echo $name >> $MAYBENOTUSED
+         echo $NAME >> $MAYBENOTUSED
       fi
    else
       SURENOTUSEDCOUNTER=$[$SURENOTUSEDCOUNTER +1]
-      echo $name >> $SURENOTUSED
+      echo $NAME >> $SURENOTUSED
    fi
-    echo -en "\rNot used: $SURENOTUSEDCOUNTER   Probably not used: $MAYBENOTUSEDCOUNTER  [ $[$SURENOTUSEDCOUNTER+$MAYBENOTUSEDCOUNTER] / $NOTFOUNDCOUNTER ]"
+    echo -en "\rNot used: $SURENOTUSEDCOUNTER   Might not be used: $MAYBENOTUSEDCOUNTER  [ $[$SURENOTUSEDCOUNTER+$MAYBENOTUSEDCOUNTER] / $NOTFOUNDCOUNTER ]"
 done < $NOTFOUNDFILE
 echo
 echo
-echo total that really look like they\'re not used: $SURENOTUSEDCOUNTER
-echo total that may not be used: $MAYBENOTUSEDCOUNTER
+echo Total that really look like they\'re not used: $SURENOTUSEDCOUNTER
+echo Total that might not be used: $MAYBENOTUSEDCOUNTER
